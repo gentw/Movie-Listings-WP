7cdfab8a5d2e4f11db37bb26a0b9681334945d4d
gentw <gent@bitemybytes.com>
2019-10-10 00:44:35 +0200

    first steps of creating plugin

---
+++ README.md
@@ -0,0 +1,1 @@
+Movie-Listings-WP
---
+++ movie-listings.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Movie Listings
+ * Description: List movies and info
+ * Version: 1.0
+ * Author: Gentian Nuka
+ **/
+
+if(!defined('ABSPATH')){
+	exit;
+}
+
+
+require_once(plugin_dir_path(__FILE__) . '/inc/movie-listings-scripts.php');
+
+
+//require_once(plugin_dir_path(__FILE__) . '/inc/movie-listings-cpt.php');
